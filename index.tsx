fb75a031fc0b200be998211d3f7dddac10610729
jamilaf0 <jamilafitrianingsih131@gmail.com>
2025-12-18 18:19:05 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,1 @@
 
-// React app logic removed in favor of multi-page HTML structure.
-// This file is now a placeholder or can be used for global ES6 module imports if needed.
-console.log("Aplikasi Pembelajaran Ikatan Kimia - Multi-page Mode");
